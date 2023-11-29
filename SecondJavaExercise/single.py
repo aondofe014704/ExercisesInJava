922068e2cc3b17b879510d4c5ed4532973beb124
aondofe014704 <songujack@gmail.com>
2023-11-29 09:08:19 +0100

    Jennifer's Assignment

---
+++ SecondJavaExercise/single.py
@@ -0,0 +1,28 @@
+print('Display "Hi" in Quotes')
+
+
+name = 10/3
+print(name)
+
+slash = 10//3
+print (slash)
+
+
+slash3 = 0/10
+print(slash3)
+
+slash4 = 0//10
+print(slash4)
+
+print("""Display "Hello Mary" and 'Bye' in quotes""")
+
+
+triple_quoted = """This ia a tripple - quoted string that spans two lines"""
+
+print(triple_quoted)
+
+name = input ("what's your name?")
+
+print(print) 
+value1 = input('Enter first number: ')
+value2 = input('Enter second number: ')
